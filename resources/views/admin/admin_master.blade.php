ee30dc52ef35a828d5b1c4bef5b8777235360f60
ricardo fuentes <ricarmenta@gmail.com>
2022-08-22 16:59:02 -0800

    FINISH EDIT

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -75,10 +75,6 @@
         <script src=" {{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
         <script src=" {{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
 
-
-        <!-- apexcharts -->
-        <script src=" {{ asset('backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-
         <!-- jquery.vectormap map -->
         <script src=" {{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
         <script src=" {{ asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}"></script>
